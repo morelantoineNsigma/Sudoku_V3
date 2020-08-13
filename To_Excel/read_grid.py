c839100e48c641cdfc192ae2224b92418c1a04a5
Antoine Morel <antoine.morel1@grenoble-inp.org>
2020-08-13 16:06:58 +0200

    lecture fichier excel

---
+++ To_Excel/read_grid.py
@@ -0,0 +1,26 @@
+import xlrd
+
+
+def read_grid(file, sheet=0, row=0, col=0):
+	grille = [[0 for _ in range(9)] for j in range(9)]
+
+	wb = xlrd.open_workbook(file)
+	s = wb.sheet_by_index(0)
+
+	for i in range(9):
+		for j in range(9):
+			value = s.cell_value(row + i, col + j)
+			try:
+				grille[i][j] = int(value)
+			except ValueError:
+				pass
+
+	return grille
+
+
+
+
+
+if __name__ == '__main__':
+	grille = read_grid("sudoku_test.xlsx", row=1, col=1)
+	print(grille)
